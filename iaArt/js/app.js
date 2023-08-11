e083fd679084aa53db6e424f5339e7cf08d28d0a
Paco <paco@mastermind.ac>
2023-08-11 10:37:54 +0200

    PopOver

--- iaArt/js/app.js
+++ iaArt/js/app.js
@@ -1,3 +1,7 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+//Inicialización del componente Carousel
 const myCarouselElement = document.querySelector('#carouseliaArt')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
